756a0c4b0de031ab1262bf14cd116ab3617e18c5
UVTDX <thevagabond@tuta.io>
2024-08-30 04:19:07 +0300

    .

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for CGoL
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
--- main.hs
+++
@@ -1,9 +0,0 @@
--- clear screen
-cls :: IO ()
-cls = putStr "\ESC[2J"
-
--- entry point
-main :: IO ()
-main = do
-	cls
-	putStrLn "Testing"
