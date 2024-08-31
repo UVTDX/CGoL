3d2fe4bda4174835c6b7279689b809361dc1632a
UVTDX <thevagabond@tuta.io>
2024-08-31 06:53:20 +0300

    .

--- src/Main.hs
+++ src/Main.hs
@@ -5,17 +5,23 @@ import System.Random (randomRIO)
 universe :: Array (Int, Int) Int
 universe = array ((1,1), (30,30)) [((row, col), 0) | row <- [1..30], col <- [1..30]]
 
-
 -- crafting the lands
 spawn :: Array (Int, Int) Int -> IO ()
 spawn arr = mapM_ putStrLn [show [arr ! (row, col) | col <- [1..30]] | row <- [1..30]]
 
+-- randomly set alive points
+randomLifeForm :: Array (Int, Int) Int -> IO (Array (Int, Int) Int)
+randomLifeForm arr = do
+  indices <- mapM (\_ -> (,) <$> randomRIO (1, 30) <*> randomRIO (1, 30)) [1..100]
+  let arr' = arr // [(idx, 1) | idx <- indices]
+  return arr'
+
 main :: IO ()
 main = do
   putStr "\ESC[2J" -- clearing the canvas
-  spawn universe
+  initialUniverse <- randomLifeForm universe
+  spawn initialUniverse
 
--- randomly set alive points
 -- check if a point is alive
 -- return set of living neighbors
 -- kill function
