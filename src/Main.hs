ec1daf1353acf5740ce6b9dbb4441d014a16cb57
UVTDX <thevagabond@tuta.io>
2024-08-31 06:15:31 +0300

    .

--- src/Main.hs
+++ src/Main.hs
@@ -2,16 +2,17 @@ import Data.Array
 
 -- imagining our utopia
 universe :: Array (Int, Int) Int
-universe = array ((1,1), (12,12)) [((row, col), 0) | row <- [1..12], col <- [1..12]]
+universe = array ((1,1), (30,30)) [((row, col), 0) | row <- [1..30], col <- [1..30]]
 
 
 -- crafting the lands
 spawn :: Array (Int, Int) Int -> IO ()
-spawn arr = mapM_ putStrLn [show [arr ! (row, col) | col <- [1..12]] | row <- [1..12]]
+spawn arr = mapM_ putStrLn [show [arr ! (row, col) | col <- [1..30]] | row <- [1..30]]
 
 -- idek fr
 main :: IO ()
 main = do
+  putStr "\ESC[2J" -- clearing the canvas
   spawn universe
 
 
