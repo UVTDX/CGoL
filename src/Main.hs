170cc368466255ee85d62fc029dcf7769064c1b0
UVTDX <thevagabond@tuta.io>
2024-08-31 05:58:30 +0300

    .

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,15 @@
 import Data.Array
 
+-- imagining our utopia
 universe :: Array (Int, Int) Int
 universe = array ((1,1), (12,12)) [((row, col), 0) | row <- [1..12], col <- [1..12]]
 
 
-
+-- crafting the lands
 spawn :: Array (Int, Int) Int -> IO ()
 spawn arr = mapM_ putStrLn [show [arr ! (row, col) | col <- [1..12]] | row <- [1..12]]
 
+-- idek fr
 main :: IO ()
 main = do
 	spawn universe
