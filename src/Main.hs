fb2e685f65f910b6526836440229e55b210dddb1
UVTDX <thevagabond@tuta.io>
2024-08-31 05:47:42 +0300

    .

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,18 @@
-TODO:
-create array
-print the array
-set random alive cells
-create neighbor functions
-create birth/death function
+--TO DO
+--set random alive cells
+--create neighbor functions
+--create birth/death function
+
+import Data.Array
+
+universe :: Array (Int, Int) Int
+universe = array ((1,1), (12,12)) [((row, col), 0) | row <- [1..12], col <- [1..12]]
+
+
+
+spawn :: Array (Int, Int) Int -> IO ()
+spawn arr = mapM_ putStrLn [show [arr ! (row, col) | col <- [1..12]] | row <- [1..12]]
+
+main :: IO ()
+main = do
+	spawn universe
